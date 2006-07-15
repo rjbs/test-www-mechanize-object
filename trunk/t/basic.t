3e04a7d629722e3cc5196d1a4898fca443817f35
hdp <hdp@1bcdbe44-fcfd-0310-b51b-975661d93aa0>
2006-07-15 03:08:16 +0000

    r25920@vex:  hdp | 2006-07-14 23:08:08 -0400  re-run basic tests with different params for 'remote' tests
    git-svn-id: svn+ssh://svn.pobox.com/svn/Test-WWW-Mechanize-Object@21881 1bcdbe44-fcfd-0310-b51b-975661d93aa0

--- trunk/t/basic.t
+++ trunk/t/basic.t
@@ -3,6 +3,7 @@
 use strict;
 use warnings;
 
+use lib 'lib';
 use lib 't/lib';
 use Test::More 'no_plan';
 
@@ -15,26 +16,36 @@ my $mech = Test::WWW::Mechanize::Object->new(
 
 isa_ok $mech, 'Test::WWW::Mechanize::Object';
 
-my $response = $mech->get('http://localhost', "get no pie");
-isa_ok $response, 'HTTP::Response';
+my $i;
+my $HOST = "http://localhost";
+TESTS: {
+  $mech->get_ok($HOST || "/", "get no pie");
 
-$mech->content_like(qr/to nowhere/, "got nowhere");
-$mech->content_like(qr/a void pie/, "got a void pie");
+  $mech->content_like(qr/to nowhere/, "got nowhere");
+  $mech->content_like(qr/a void pie/, "got a void pie");
+  
+  $mech->get_ok(
+    "$HOST/kitchen?pie=cherry", 
+    "get cherry pie"
+  );
+  
+  $mech->content_like(qr{to /kitchen},  "got to the kitchen");
+  $mech->content_like(qr{a cherry pie}, "got a cherry pie");
+  
+  $mech->get_ok(
+    "$HOST/windowsill?pie=random",
+    "get random pie (redirect)"
+  );
+  
+  $mech->content_like(qr{to /windowsill}, "path preserved");
+  $mech->content_unlike(qr{a random pie}, "no longer random pie");
+  $mech->content_unlike(qr{a void pie},   "not a void pie either");
 
-$mech->get_ok(
-  "http://localhost/kitchen?pie=cherry", 
-  "get cherry pie"
-);
-
-$mech->content_like(qr{to /kitchen},  "got to the kitchen");
-$mech->content_like(qr{a cherry pie}, "got a cherry pie");
-
-$mech->get_ok(
-  "http://localhost/windowsill?pie=random",
-  "get random pie (redirect)"
-);
-
-$mech->content_like(qr{to /windowsill}, "path preserved");
-$mech->content_unlike(qr{a random pie}, "no longer random pie");
-$mech->content_unlike(qr{a void pie},   "not a void pie either");
-
+  unless ($i++) {
+    # switch to remote-possible mode and try them all again
+    $ENV{TWMO_SERVER} = 'http://myserver.com/myurl';
+    $HOST = "";
+    $mech->{handler} = TWMO::Remote->new;
+    redo TESTS;
+  }
+}
--- trunk/t/lib/TWMO.pm
+++ trunk/t/lib/TWMO.pm
@@ -15,27 +15,69 @@ sub request {
 
   if ($query->{pie} && $query->{pie} eq 'random') {
     my $response = HTTP::Response->new(302);
-    $response->header(
-      Location => 'http://localhost' .
-        ($request->uri->path || '/') .
-          '?pie=' . $pies[rand @pies]
-        );
+    my $location = $request->uri->clone;
+    $location->query_form(pie => $pies[rand @pies]);
+    $response->header( Location => $location );
     return $response;
   } 
 
   my $response = HTTP::Response->new(200);
+  my $uri = $request->uri->canonical;
   $response->content(
-    sprintf <<"END",
+    sprintf(
+      <<"END",
+Your host is %s.
 You got to %s.
 You asked for a %s pie.
 END
-    $request->uri->path || "nowhere",
-    $query->{pie} || "void",
+      $uri->host,
+      ($uri->path eq '/' ? "nowhere" : $uri->path),
+      $query->{pie} || "void",
+    )
   );
   return $response;
 }
 
-package TWMO::Rebasing;
+package TWMO::Remote;
 
+use URI;
+our @ISA = qw(TWMO);
+my $DEFAULT = URI->new("http://localhost")->canonical;
+
+sub prepare_request {
+  my ($self, $request) = @_;
+  my $uri = $request->uri;
+  my $twmo = URI->new($ENV{TWMO_SERVER} || return);
+
+  if ($uri->scheme eq $DEFAULT->scheme and
+        $uri->host eq $DEFAULT->host) {
+    $uri->scheme($twmo->scheme);
+    $uri->host($twmo->host);
+    my $path = $twmo->path . '/' . $uri->path;
+    $path =~ s{/+}{/}g;
+    $uri->path($path);
+
+    $request->uri($uri->canonical);
+  }
+  main::diag($request->uri);
+}
+
+sub before_request {
+  my ($self, $request) = @_;
+  my $uri = $request->uri;
+  my $twmo = URI->new($ENV{TWMO_SERVER} || return);
+
+  if ($uri->scheme eq $twmo->scheme and
+        $uri->host eq $twmo->host and
+          $uri->path =~ /^\Q@{[ $twmo->path ]}\E/) {
+    $uri->scheme($DEFAULT->scheme);
+    $uri->host($DEFAULT->host);
+    my $path = $uri->path;
+    $path =~ s,^\Q@{[ $twmo->path ]}\E/*,,;
+    $uri->path($path);
+    $request->uri($uri);
+  }
+  main::diag($uri);
+}
 
 1;
